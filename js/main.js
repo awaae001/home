8ee169a09af1c8cc9b2a0d1028fe2e538833ff62
awaae001 <3271436144@qq.com>
2024-03-11 03:19:32 +0800

    upd:完工!

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
+function opensiteshow() {
+  document.getElementById('support-dialog').style.display = 'block';
+}
+function closesiteshow() {
+  document.getElementById('support-dialog').style.display = 'none';
+}
+function openabotmeshow() {
+  document.getElementById('about-me').style.display = 'block';
+}
+function closeabotmeshow() {
+  document.getElementById('about-me').style.display = 'none';
+}
+
+
+
 function showTime() {
     var date = new Date();
     var hour = date.getHours();
     var minute = date.getMinutes();
 
     var element = document.getElementById('time');
-    element.innerHTML = hour + '点' + minute +'分'
+    element.innerHTML = hour + ':' + minute
 }
 setInterval('showTime()', 1000);
 
-/** 
+
 fetch('https://v1.hitokoto.cn')
 .then(function (res){
   return res.json();
@@ -20,5 +35,5 @@ fetch('https://v1.hitokoto.cn')
 .catch(function (err) {
   console.error(err);
 })
-*/
-  
+
+
