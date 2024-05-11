e6dcbe122216bc8e6543dca1fe63b10bbf0d0662
awaae001 <3271436144@qq.com>
2024-05-11 23:54:10 +0800

    短点：主页修复

--- js/main.js
+++ js/main.js
@@ -11,18 +11,6 @@ function closeabotmeshow() {
   document.getElementById('about-me').style.display = 'none';
 }
 
-
-function showTime() {
-    var date = new Date();
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-
-    var element = document.getElementById('time');
-    element.innerHTML = hour + ':' + minute
-}
-setInterval('showTime()', 1000);
-
-
 fetch('https://v1.hitokoto.cn')
 .then(function (res){
   return res.json();
